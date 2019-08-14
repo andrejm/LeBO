a94e8d8988a59791bc83420b66bb62d9e9d11844
Andrej <hello@andrzej.sk>
2019-08-14 11:18:04 +0200

    add responsive header that hides on scroll and shows on scrolling up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,51 @@ require('foundation-sites');
 
 
 $(document).foundation();
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+var currentScrollTop = 0,
+	$header = $( '.header' ),
+	$site = $( '.site' );
+var headerHeight = $header.outerHeight();
+
+var fixHeader = debounce( function() {
+
+	$header.toggleClass( 'header--fixed header--hidden', ( $(window).scrollTop() < currentScrollTop ) );
+	currentScrollTop = $(window).scrollTop();
+
+	if( currentScrollTop <= 0 ) {
+		$header.removeClass( 'header--fixed header--hidden');
+		$site.css('margin-top', 0);
+	} else if ( currentScrollTop > 20 ) {
+		$header.addClass( 'header--hidden');
+		$site.css('margin-top', headerHeight);
+	}
+	// console.log( 'fixed, ' + currentScrollTop );
+	// console.log( headerHeight );
+}, 100 );
+
+
+jQuery(document).ready(function($) {
+	$(window).on( 'scroll', fixHeader );
+	$(window).on( 'resize', debounce( function() {
+		headerHeight = $header.outerHeight();
+	}, 150 ));
+});
